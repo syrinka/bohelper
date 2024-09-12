2caf39bed5fb8b20f2e547fc8dc8f94bd12c15f7
syrinka <syrinka@foxmail.com>
2024-09-12 23:23:52 +0800

    add `all` filter

--- bohelper.py
+++ bohelper.py
@@ -31,10 +31,35 @@ def recipe(name):
 @bohelper.command()
 @click.argument('query')
 @click.argument('tier', type=click.IntRange(0,3), default=0, required=False)
-def want(query, tier):
+@click.option('--all', '-a', type=bool, is_flag=True, default=False,
+    help='查询所有技艺的配方，默认只查询已习得的技艺。')
+def want(query, tier, all):
+    """
+    查询合成指定条件的物品的配方。
+
+    \b
+    QUERY
+    =====
+    过滤条件。例：
+      > 月
+      > 蜜4，蛾2
+      > 预兆，灯，心3
+    将会显示查询性向高于条件的物品。
+
+    \b
+    TIER
+    ====
+    配方等级。可选取值与意义分别为：
+    0 - 所有配方（默认值）
+    1 - 入门级【5】
+    2 - 学者级【10】
+    3 - 秘授级【15】
+    """
     hold = {}
     cond = parse_condition(query)
     for skill in SKILLS.values():
+        if not (all or skill['ID'] in P.skills):
+            continue
         label = skill['Label']
         for recipe in skill['recipes']:
             if tier != 0 and recipe['tier'] != tier:
@@ -60,7 +85,9 @@ def want(query, tier):
 
 @bohelper.command()
 @click.argument('principles', nargs=-1)
-def wantmem(principles):
+@click.option('--all', '-a', type=bool, is_flag=True, default=False,
+    help='查询所有书籍的回忆，默认只查询已掌握的书籍。')
+def wantmem(principles, all):
     for tome in TOMES.values():
         mem = ITEMS[tome['memory']]
         for p in principles:
@@ -75,15 +102,19 @@ def wantmem(principles):
 def load(file):
     save = json.load(open(file, encoding='utf-8'))
     raw = json.dumps(save['RootPopulationCommand']['Spheres'])
-    tomes = []
+
+    P.tomes.clear()
     for book in save['CharacterCreationCommands'][0]['UniqueElementsManifested']:
         start = raw.find(book, 5000)
         end = raw.find('Illuminations', start)
         if 'mastery' in raw[start:end]:
-            tomes.append(book)
-    for i in tomes:
-        print(i)
-    P.tomes = tomes
+            P.tomes.append(book)
+
+    P.skills.clear()
+    for skill in SKILLS.keys():
+        if raw.find(skill, 5000) != -1:
+            P.skills.append(skill)
+
     P.save()
 
 
